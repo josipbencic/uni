0bbfdc38fd4b8c4cd842d513ddc5afda4a9f1011
Mladen Jurak <jurak@math.hr>
2017-12-24 21:25:21 +0100

    cmake - src

---
+++ src/zadatak.cpp
@@ -0,0 +1,14 @@
+#include "zadatak.h"
+
+void zadatak1(){
+  // Implementacija zadatka 1
+}
+
+void zadatak2(){
+  // Implementacija zadatka 2
+}
+
+void zadatak3(){
+  // Implementacija zadatka 3
+}
+
---
+++ src/zadatak.h
@@ -0,0 +1,6 @@
+// Ubaciti potrebne include datoteke
+
+void zadatak1();
+void zadatak2();
+void zadatak3();
+
