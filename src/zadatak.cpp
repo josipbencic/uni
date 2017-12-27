08b01baa04422146cd2134b62b06cf86c947b75a
Mladen Jurak <jurak@math.hr>
2017-12-27 21:07:53 +0100

    Fixing testing.

--- src/zadatak.cpp
+++ src/zadatak.cpp
@@ -1,14 +1,18 @@
 #include "zadatak.h"
 // Ubaciti potrebne include datoteke
+#include <iostream>
 #include <fstream>
 #include <algorithm>
 #include <vector>
 #include <iterator>
 #include <list>
+#include <stdexcept>
 
 std::vector<int> zadatak1(std::string const & file_name){
   // Implementacija zadatka 1
    std::ifstream in(file_name);
+   if(!in)
+       throw std::runtime_error("File "+ file_name +" not found.");
    std::istream_iterator<int> isi_in(in);
    std::istream_iterator<int> isi_end;    
 
@@ -25,22 +29,45 @@ void zadatak2(){
 std::list<int> zadatak3(){
   // Implementacija zadatka 3
 	std::list<int> l1{1,2,3,4,5,6,7,8,9};
-	std::list<int> l2 = l1;
-	while( *l1.begin() != 5 )
+    std::list<int> l2{l1};
+    while( *l1.begin() != 5 ){
 		std::next_permutation(l1.begin(), l1.end());
+    }
+//    std::cout << "l1 : ";
+//    for(auto const & x : l1) std::cout << x << ",";
+//    std::cout << std::endl;
+
 	while( *l2.begin() != 9 )
 		std::next_permutation(l2.begin(), l2.end());
 
-	auto it1 = std::find_if(l2.begin(), l2.end(), [](int x){ return ((x == 4) or (x == 7)); });
+//    std::cout << "l2 : ";
+//    for(auto const & x : l2) std::cout << x << ",";
+//    std::cout << std::endl;
+
+    auto f = [](int &x){ return ((x == 4) || (x == 7)); };
+    auto it1 = std::find_if(l2.begin(), l2.end(), f);
 	if(it1 == l2.end()) throw std::runtime_error("First element not found.");
-	auto it1p = it1++;
-	auto it2 = std::find_if(it1p, l2.end(), [](int x){ return ((x == 4) or (x == 7)); });
+    it1++;
+    auto it2 = std::find_if(it1, l2.end(), [](int &x){ return ((x == 4) || (x == 7)); });
 	if(it2 == l2.end()) throw std::runtime_error("Second element not found.");
+    it1--;
 
 	auto it = std::find(l1.begin(), l1.end(), 3);
 	if(it == l1.end()) throw std::runtime_error("Element 3 not found.");
 
+//    std::cout << "*it1 = "  << *it1 << std::endl;
+//    std::cout << "*it2 = "  << *it2 << std::endl;
+//    std::cout << "*it = "  << *it << std::endl;
+
 	l1.splice(it, l2, it1, it2);
+//    std::cout << "l1 : ";
+//	for(auto const & x : l1) std::cout << x << ",";
+//	std::cout << std::endl;
+
+//    std::cout << "l2 : ";
+//    for(auto const & x : l2) std::cout << x << ",";
+//    std::cout << std::endl;
+
 	return l1;
 }
 
--- test/test-zad.cpp
+++ test/test-zad.cpp
@@ -2,20 +2,43 @@
 #include "../src/zadatak.h"
 #include <vector>
  
-TEST(Zadatak1, Vrijednosti) { 
-	std::vector<int> vec = zadatak1("../../src/brojevi.txt");
+TEST(STL, Zadatak1) {
+    std::vector<int> vec = zadatak1("../src/brojevi.txt");
 	
-    ASSERT_EQ (vec.size(), 19);
-    ASSERT_EQ (vec[0], 1);
-    ASSERT_EQ (vec[18], 19);
-    ASSERT_EQ (vec[10], 9);
+    ASSERT_EQ ((int)vec.size(), 19);
+    ASSERT_EQ (vec[0], 1) << vec[0] << " <> 1";
+    ASSERT_EQ (vec[18], 19)<< vec[18] << " <> 19";
+    ASSERT_EQ (vec[10], 9)<< vec[10] << " <> 9";
 
 }
 
-TEST(Zadatak3, Vrijednosti) { 
+TEST(STL, Zadatak3) {
 
 	std::list<int> l = zadatak3();
-	
+    auto it = l.begin();
+    ASSERT_EQ(*it, 5);
+    it++;
+    ASSERT_EQ(*it, 1);
+    it++;
+    ASSERT_EQ(*it, 2);
+    it++;
+    ASSERT_EQ(*it, 4);
+    it++;
+    ASSERT_EQ(*it, 5);
+    it++;
+    ASSERT_EQ(*it, 6);
+    it++;
+    ASSERT_EQ(*it, 3);
+    it++;
+    ASSERT_EQ(*it, 4);
+    it++;
+    ASSERT_EQ(*it, 6);
+    it++;
+    ASSERT_EQ(*it, 7);
+    it++;
+    ASSERT_EQ(*it, 8);
+    it++;
+    ASSERT_EQ(*it, 9);
 }
 
 int main(int argc, char **argv) {
