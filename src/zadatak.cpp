06c83416d2e1e811131814be7e1593e7e8c67f8e
Josip Bencic <josipbencic@outlook.com>
2018-03-14 16:57:47 +0100

    Gotov.

--- src/zadatak.cpp
+++ src/zadatak.cpp
@@ -1,8 +1,9 @@
 #include "zadatak.h"
 // Ubaciti potrebne include datoteke
 
+#include <cmath>
+
 double zadatak(double radijus){
   // Implementacija zadatka
- 
+  return 2.0 * radijus * M_PI;
 }
-
