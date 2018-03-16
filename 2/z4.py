e9fe37987b79eee4db162eda513762971cb11adc
josipbencic <josipbencic@outlook.com>
2018-03-16 17:07:09 +0100

    solved z5 with python

--- 2/z4.py
+++ 2/z4.py
@@ -2,8 +2,14 @@ import numpy as np
 from numpy import linalg as la
 
 def solve():
-  pam1 = np.loadtxt("PAM1.txt")
-  np.linalg.matrix_power(pam1, 1000)
-  print(pam1)
+  raw = np.loadtxt("PAM1.txt", np.float64)
+  pam1 = np.multiply(1/10000, raw)
+  #print(pam1)
+  a =  la.matrix_power(pam1, 1000)
+  
+  for i in range(0, len(a)):
+    for j in range(0, len(a[i])):
+      print(a[i][j])
+    print("")
 
 solve()
