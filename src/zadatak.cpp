c0120b36c31f398c3e31398580797c95b770f6b7
Josip Bencic <josipbencic@outlook.com>
2018-03-21 16:00:13 +0100

    z1 gotov; z2 WIP

--- src/zadatak.cpp
+++ src/zadatak.cpp
@@ -1,15 +1,56 @@
 #include "zadatak.h"
 // Ubaciti potrebne include datoteke
+#include <iterator>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <list>
+#include <stdexcept>
+#include <algorithm>
+#include <iomanip>
+#include <cmath>
 
-std::vector<int> zadatak1(std::string const & file_name){
+
+#include <iostream>
+
+std::vector<int> zadatak1(std::string const & file_name) {
   // Implementacija zadatka 1
+  std::ifstream in(file_name);
+  if (!in) {
+    throw std::runtime_error(std::string("No file named") + file_name);
+  }
+  std::istream_iterator<int> fit(in);
+  std::istream_iterator<int> fit_end;
+  std::vector<int> ret(fit, fit_end);
+  std::replace(ret.begin(), ret.end(), 0, 9);
+  return ret;
 }
 
-void zadatak2(std::string const & file_name){
+void zadatak2(std::string const & file_name) {
   // Implementacija zadatka 2
+  std::ifstream in(file_name);
+  if (!in) {
+    throw std::runtime_error(std::string("No file named") + file_name);
+  }
+  std::istream_iterator<int> fit(in);
+  std::istream_iterator<int> fit_end;
+  std::vector<int> v;
+  std::copy(fit, fit_end, std::back_inserter(v));
+
+  for (auto x : v) std::cout << x;
+  std::cout << std::endl;
+
+  std::ofstream out("3cols.txt");
+  if (!out) {
+    throw std::runtime_error("Cannot create 3cols.txt!");
+  }
+  out << std::setprecision(6) << std::showpos << std::scientific;
+  for (auto const & x : v) {
+    out << x << " " << sin(x) << " " << exp(x) << "\n";
+  }
 }
 
-std::list<int> zadatak3(){
+std::list<int> zadatak3() {
   // Implementacija zadatka 3
 }
 
