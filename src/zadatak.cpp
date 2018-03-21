9d11424b2401b8a7eea2559b18d02b97c1290163
Josip Bencic <josipbencic@outlook.com>
2018-03-21 16:50:28 +0100

    Gotov.

--- src/zadatak.cpp
+++ src/zadatak.cpp
@@ -9,9 +9,7 @@
 #include <algorithm>
 #include <iomanip>
 #include <cmath>
-
-
-#include <iostream>
+#include <cassert>
 
 std::vector<int> zadatak1(std::string const & file_name) {
   // Implementacija zadatka 1
@@ -43,11 +41,34 @@ void zadatak2(std::string const & file_name) {
   }
   out << std::setprecision(6) << std::showpos << std::scientific;
   for (auto const & x : v) {
-    out << x << " " << sin(x) << " " << exp(x) << "\n";
+    out << x << " " << std::sin(x) << " " << std::exp(x) << "\n";
   }
 }
 
 std::list<int> zadatak3() {
   // Implementacija zadatka 3
+  std::list<int> l1{1,2,3,4,5,6,7,8,9};
+  std::list<int> l2{1,2,3,4,5,6,7,8,9};
+  while ((*l1.begin()) != 5 && std::next_permutation(l1.begin(), l1.end()));
+  while ((*l2.begin()) != 9 && std::next_permutation(l2.begin(), l2.end()));
+
+  auto it = find(l1.begin(), l1.end(), 3);
+  assert(it != l1.end());
+
+  auto it2_7 = find(l2.begin(), l2.end(), 7);
+  assert(it2_7 != l2.end());
+
+  auto it2_4 = find(l2.begin(), l2.end(), 4);
+  assert(it2_4 != l2.end());
+
+  if (std::distance(it2_7, it2_4) < 0) {
+    // 7 comes before 4
+    l1.splice(it, l2, it2_7, it2_4);
+  }
+  else {
+    //  4 comes before 7
+    l1.splice(it, l2, it2_4, it2_7);
+  }
+  return l1;
 }
 
