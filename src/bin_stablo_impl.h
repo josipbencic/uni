a3c03c562622d7ae22b61e80581eca9351398a9e
jurak <jurak@math.hr>
2018-04-10 12:59:10 +0200

    Initial commit.

---
+++ src/bin_stablo_impl.h
@@ -0,0 +1,18 @@
+#ifndef BIN_STABLO_IMPL_H_INCLUDED
+#define BIN_STABLO_IMPL_H_INCLUDED
+
+///// Implementacija metoda iz Node klase //////////////////////
+
+
+
+//// Implementacija metoda iz BinaryTree klase //////////////////
+
+template <typename T>
+template <typename Funct>
+void BinarySTree<T>::preOrder(Node<T> * root, Funct visit)
+{
+     throw "Not implemented.";   
+}
+
+
+#endif // BIN_STABLO_IMPL_H_INCLUDED
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "bin_stablo.h"
+
+
+int main()
+{
+    // Vlastiti testovi
+    throw "Not implemented.";
+    return 0;
+}
